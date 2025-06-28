54ddf647c650b99c83a609a2236cc0707d5d3875
Abdul <Tengdana45@gmail.com>
2025-06-28 00:43:46 -0400

    Allow SSH from any IP for testing

--- Project3/terraform/main.tf
+++ Project3/terraform/main.tf
@@ -79,10 +79,7 @@ resource "aws_security_group" "web_sg" {
   from_port   = 22
   to_port     = 22
   protocol    = "tcp"
-  cidr_blocks = [
-    "104.158.105.134/32",
-    "140.82.112.0/20"       
-  ]
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
   ingress {
