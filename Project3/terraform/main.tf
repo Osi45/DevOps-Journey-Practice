afb471088de3f771cd3add82fd31e42395ea8428
Abdul <Tengdana45@gmail.com>
2025-06-30 13:46:54 -0400

    Refactor: use pre-generated SSH key instead of Terraform-generated key

--- Project3/terraform/main.tf
+++ Project3/terraform/main.tf
@@ -197,11 +197,6 @@ resource "aws_instance" "monitoring" {
   }
 }
 
-output "private_key" {
-  value     = tls_private_key.this.private_key_pem
-  sensitive = true
-}
-
 output "web_app_public_ip" {
   value = aws_instance.web_app.public_ip
 }
