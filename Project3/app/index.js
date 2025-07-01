703d5145935dc838791924d52187328147b080b2
Abdul <Tengdana45@gmail.com>
2025-06-30 22:47:39 -0400

    Change app port from 3000 to 4000

--- Project3/app/index.js
+++ Project3/app/index.js
@@ -54,7 +54,7 @@ app.get('/health', (req, res) => {
   res.json({ status: 'OK', timestamp: new Date().toISOString() });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, '0.0.0', (err) => {
   if (err) {
