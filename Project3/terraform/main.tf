72a1cb63f464be4ef05f1b4cb899dfee941205ec
Abdul <Tengdana45@gmail.com>
2025-07-04 21:52:37 -0400

    Fixed terraform.tfvars syntax

--- Project3/terraform/main.tf
+++ Project3/terraform/main.tf
@@ -39,10 +39,6 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_eip" "nat_eip" {
-  vpc = true
-}
-
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_subnet.id
