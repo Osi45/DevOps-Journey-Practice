c447f887f457b2ae98bde114bb7bb9633e397d92
Abdul <Tengdana45@gmail.com>
2025-07-15 16:40:36 -0400

    Updated IP using my mac machine to allow access

--- Project3/terraform/security_groups.tf
+++ Project3/terraform/security_groups.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 4000
     to_port     = 4000
     protocol    = "tcp"
-    cidr_blocks = ["104.158.105.134/32"]  
+    cidr_blocks = ["65.92.152.43/32"]  
   }
 
   egress {
@@ -38,28 +38,28 @@ resource "aws_security_group" "monitoring_sg" {
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
-    cidr_blocks = ["104.158.105.134/32"]  
+    cidr_blocks = ["65.92.152.43/32"]  
   }
 
   ingress {
     from_port   = 5601
     to_port     = 5601
     protocol    = "tcp"
-    cidr_blocks = ["104.158.105.134/32"]  
+    cidr_blocks = ["65.92.152.43/32"]  
   }
 
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["104.158.105.134/32"]
+    cidr_blocks = ["65.92.152.43/32"]
   }
 
   ingress {
     from_port   = 9200
     to_port     = 9200
     protocol    = "tcp"
-    cidr_blocks = ["104.158.105.134/32"]
+    cidr_blocks = ["65.92.152.43/32"]
   }
 
   egress {
